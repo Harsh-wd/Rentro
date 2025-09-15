5164bf2aee3a1a6c4f41eea469f2aa6eed9fa8c8
Harsh-wd <harshbansal.it26@gmail.com>
2025-09-15 23:18:32 +0530

    components updated

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,5 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { assets, cityList } from '../assets/assets';
+import React, { useState, useEffect } from 'react'
+import { assets, cityList } from '../assets/assets'
+import { useAppContext } from '../context/AppContext'
+import {motion} from 'motion/react'
+
 
 const carImages = [
   assets.main_car,
@@ -11,6 +14,13 @@ const Hero = () => {
 const [currentIndex, setCurrentIndex] = useState(0);
 const[pickupLocation,setPickupLocation]=useState('')
 
+const {pickupDate,setPickupDate,returnDate,setReturnDate,navigate}=useAppContext()
+
+const handleSearch = (e)=>{
+  e.preventDefault()
+  navigate('/cars?pickupLocation='+pickupLocation + '&pickupDate='+pickupDate + '&returnDate=' + returnDate)
+}
+
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -20,13 +30,24 @@ const[pickupLocation,setPickupLocation]=useState('')
   }, []);
 
   return (
-    <div className='h-screen flex flex-col items-center justify-center gap-14 bg-light text-center px-4 relative overflow-hidden'>
-      <h1 className='text-4xl md:text-5xl font-semibold z-10'>
+    <motion.div 
+    initial={{opacity:0}}
+    animate={{opacity:1}}
+    transition={{duration:0.8}}
+    className='h-screen flex flex-col items-center justify-center gap-14 bg-light text-center px-4 relative overflow-hidden'>
+      <motion.h1 initial={{y:50,opacity:0}}
+          animate={{y:0,opacity:1}}
+          transition={{duration:0.8,delay:0.2}}
+      className='text-4xl md:text-5xl font-semibold z-10'>
         Take Any Vehicle On Rent
-      </h1>
+      </motion.h1>
 
      
-      <form className='flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-1g md:rounded-full
+      <motion.form 
+      initial={{scale:0.95,opacity:0,y:50}}
+      animate={{scale:1,opacity:1,y:0}}
+      transition={{duration:0.6,delay:0.4}}
+      onSubmit={handleSearch} className='flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-1g md:rounded-full
        w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]'>
         <div className='flex flex-col md:flex-row items-start md:items-center
         gap-10 min-md:ml-8'>
@@ -43,24 +64,28 @@ const[pickupLocation,setPickupLocation]=useState('')
           </div>
           <div className='flex flex-col items-start gap-2'>
             <label htmlFor="pickup-date">Pick-up Date</label>
-            <input type="date" id="pickup-date" min={new Date().
+            <input value={pickupDate} onChange={e=>setPickupDate(e.target.value)} type="date" id="pickup-date" min={new Date().
               toISOString().split('T'[0])} className='text-sm text-gray-500' required />
           </div>
           <div className='flex flex-col items-start gap-2'>
             <label htmlFor="return-date">Return Date</label>
-            <input type="date" id="return-date" className='text-sm text-gray-500' required />
+            <input value={returnDate} onChange={e=>setReturnDate(e.target.value)}
+             type="date" id="return-date" className='text-sm text-gray-500' required />
           </div>
 
         </div >
-        <button className='flex items-center justify-center gap-1 px-9
+        <motion.button 
+        whileHover={{scale:1.05}}
+        whileTap={{scale:0.95}}
+        className='flex items-center justify-center gap-1 px-9
           py-3 max-sm:mt-4 bg-primary hover:bg-primary-dull text-white
           rounded-full cursor-pointer'>
           <img src={assets.search_icon} alt="search"
           className='brightness-300' />
           Search
-          </button>
+          </motion.button>
 
-      </form>
+      </motion.form>
 
       {/* Image Slider Container */}
       <div className="relative w-full max-w-4xl h-[250px] md:h-[400px] mt-4">
@@ -75,8 +100,8 @@ const[pickupLocation,setPickupLocation]=useState('')
           />
         ))}
       </div>
-    </div>
-  );
-};
+    </motion.div>
+  )
+}
 
 export default Hero;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { assets, menuLinks } from '../assets/assets';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useAppContext } from '../context/AppContext';
+import toast from 'react-hot-toast';
+import {motion} from 'motion/react'
 
-const Navbar = ({ setShowLogin }) => {
+const Navbar = () => {
+      const {setShowLogin,user,logout,isOwner,axios,setIsOwner}=useAppContext()
+
   const location = useLocation();
   const [open, setOpen] = useState(false);
   const navigate = useNavigate();
-
+  const changeRole = async()=>{
+    try {
+      const {data}=await axios.post('/api/owner/change-role')
+      if (data.success){
+        setIsOwner(true)
+        toast.success(data.message)
+      }else{
+        toast.error(data.message)
+      }
+  } catch (error) {
+      toast.error(error.message)
+  }
+}
   // Close menu on route change
   useEffect(() => {
-    setOpen(false);
+    setOpen(false); 
   }, [location.pathname]);
 
   // Close on Esc key
@@ -22,12 +39,16 @@ const Navbar = ({ setShowLogin }) => {
   }, []);
 
   return (
-    <div className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all 
-      ${location.pathname === '/' ? 'bg-light' : 'bg-white'}`}>
+    <motion.div 
+    initial={{y: -15,opacity:0}}
+    animate={{y:0,opacity:2}}
+    transition={{duration:0.5}}
+    className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all 
+      ${location.pathname === '/' &&'bg-light'}`}>
 
       {/* Logo */}
       <Link to='/'>
-        <img src={assets.logo} alt="logo" className='h-14' />
+        <motion.img whileHover={{scale:1.10}} src={assets.logo} alt="logo" className='h-14' />
       </Link>
 
       {/* Mobile Backdrop Overlay */}
@@ -69,18 +90,15 @@ const Navbar = ({ setShowLogin }) => {
 
         {/* Action Buttons */}
         <div className='flex max-sm:flex-col items-start sm:items-center gap-6'>
-          <button onClick={() => { navigate('/owner'); setOpen(false); }} className="cursor-pointer">
-            Dashboard
+          <button onClick={() => isOwner ?  navigate('/owner'):changeRole() }
+           className="cursor-pointer">
+            { isOwner ? 'Dashboard' : 'List Cars'}
           </button>
+
           <button
-            onClick={() => {
-              setShowLogin(true);
-              setOpen(false);
-            }}
-            className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg"
-          >
-            Login
-          </button>
+            onClick={() => {user ? logout() : setShowLogin(true)}}
+            className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg">
+           {user ? 'Logout': 'Login'}</button>
         </div>
       </div>
 
@@ -88,8 +106,8 @@ const Navbar = ({ setShowLogin }) => {
       <button className='sm:hidden z-50 relative cursor-pointer' aria-label="Menu" onClick={() => setOpen(!open)}>
         <img src={open ? assets.close_icon : assets.menu_icon} alt="menu" />
       </button>
-    </div>
-  );
-};
+    </motion.div>
+  )
+}
 
 export default Navbar;
